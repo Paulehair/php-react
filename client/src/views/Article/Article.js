6d31d606c8b4d0737d7dd600711748b4aa62c453
Thuillez <idealistick@outlook.fr>
2019-01-13 22:02:48 +0100

    add components and integration article + improve generic class

--- client/src/views/Article/Article.js
+++ client/src/views/Article/Article.js
@@ -5,19 +5,24 @@ const Article = ({ articles, goToNextOption, currentArticleIndex, slug, index })
     let code;
     if (articles) {
         const currentArticle = articles[currentArticleIndex];
-        code = <div key={currentArticle.id}>
-            <h1>{currentArticle.title}</h1>
-            <h4>{currentArticle.subtitle}</h4>
-            <p>{currentArticle.firstText}</p>
-            <p>{currentArticle.secondText}</p>
-            <button onClick={() => goToNextOption(index)} className={"button"}>{currentArticle.firstChoice}</button>
+        code = <div className={"container"} key={currentArticle.id}>
+            <h1 className={"article-title title"}>{currentArticle.title}</h1>
+            <h4 className={"article-subtitle subtitle"}>{currentArticle.subtitle}</h4>
+            <p className={"article-text"}>{currentArticle.firstText}</p>
+            <div className="article-imgContainer">
+                <img className={"article-img"} src="./images/03-article/img.png" alt=""/>
+            </div>
+            <p className={"article-text"}>{currentArticle.secondText}</p>
+            <div className="container">
+                <button onClick={() => goToNextOption(index)} className={"button"}>{currentArticle.firstChoice}</button>
+            </div>
             <input ref={index} type="hidden" defaultValue={currentArticle.firstIndex} />
             <input ref={slug} type="hidden" defaultValue={currentArticle.slug} />
         </div>;
     }
 
     return articles && (
-        <div>
+        <div className="article">
             {code}
         </div>
     );
